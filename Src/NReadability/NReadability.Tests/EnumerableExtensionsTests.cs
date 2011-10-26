4fa02ca166cc5cfbbf40f5eaad10ab3d6d7da228
Marek Stój <marek.stoj@gmail.com>
2011-10-26 20:06:26 +0200

    Added missing files.

---
+++ Src/NReadability/NReadability.Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NReadability.Tests
+{
+  [TestFixture]
+  public class EnumerableExtensionsTests
+  {
+    #region SingleOrNone() tests
+
+    [Test]
+    public void SingleOrNone_throws_if_collection_is_null()
+    {
+      // ReSharper disable ConditionIsAlwaysTrueOrFalse
+
+      Assert.Throws<ArgumentNullException>(
+        () => ((IEnumerable<string>)null).SingleOrNone());
+
+      // ReSharper restore ConditionIsAlwaysTrueOrFalse
+    }
+
+    [Test]
+    public void SingleOrNone_returns_null_if_collection_is_empty()
+    {
+      Assert.IsNull(new string[0].SingleOrNone());
+    }
+
+    [Test]
+    public void SingleOrNone_returns_the_only_element_if_collection_has_exactly_one_element()
+    {
+      Assert.AreEqual("element", new[] { "element" }.SingleOrNone());
+    }
+
+    #endregion
+  }
+}
---
+++ Src/NReadability/NReadability/EnumerableExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NReadability
+{
+  public static class EnumerableExtensions
+  {
+    /// <summary>
+    /// Returns the only one element in the sequence or default(T) if either the sequence doesn't contain any elements or it contains more than one element.
+    /// </summary>
+    public static T SingleOrNone<T>(this IEnumerable<T> enumerable)
+      where T : class
+    {
+      // ReSharper disable PossibleMultipleEnumeration
+
+      if (enumerable == null)
+      {
+        throw new ArgumentNullException("enumerable");
+      }
+
+      T firstElement = enumerable.FirstOrDefault();
+
+      if (firstElement == null)
+      {
+        // no elements
+        return null;
+      }
+
+      T secondElement = enumerable.Skip(1).FirstOrDefault();
+
+      if (secondElement != null)
+      {
+        // more than one element
+        return null;
+      }
+
+      return firstElement;
+
+      // ReSharper restore PossibleMultipleEnumeration
+    }
+  }
+}
