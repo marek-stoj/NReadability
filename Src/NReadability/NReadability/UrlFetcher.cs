938e5d0e8675fd09805559d8b975be9ed2e1e9a7
Marek Stój <marek.stoj@gmail.com>
2012-06-24 08:58:47 +0200

    Added support for cookies and compressed content to UrlFetcher.

--- Src/NReadability/NReadability/UrlFetcher.cs
+++ Src/NReadability/NReadability/UrlFetcher.cs
@@ -18,29 +18,233 @@
  * limitations under the License.
  */
 
+using System;
+using System.Collections.Specialized;
+using System.Globalization;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace NReadability
 {
-  /// <summary>
-  /// Fetches web content.
-  /// </summary>
   public class UrlFetcher : IUrlFetcher
   {
-    #region IUrlFetcher members
+    #region Nested types
+
+    private enum CompressionAlgorithm
+    {
+      GZip,
+      Deflate,
+    }
+
+    #endregion
+
+    private const int BufferSize = 8192;
+
+    private static readonly Encoding _DefaultFallbackEncoding = Encoding.UTF8;
+    private static readonly Regex _MetaTagRegex = new Regex("<meta[^>]+content=\"[^\"]*charset=(?<charset>[^\"]+)\"", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+    private readonly Encoding _fallbackEncoding;
+    private readonly CookieAwareWebClient _webClient;
+
+    #region Constructor(s)
+
+    public UrlFetcher(Encoding fallbackEncoding)
+    {
+      _fallbackEncoding = fallbackEncoding ?? _DefaultFallbackEncoding;
+
+      _webClient = new CookieAwareWebClient();
+      _webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:13.0) Gecko/20100101 Firefox/13.0.1");
+      _webClient.Headers.Add("Accept-Encoding", "gzip,deflate");
+    }
+
+    public UrlFetcher()
+      : this(null)
+    {
+    }
+
+    #endregion
+
+    #region Public methods
+
+    public string DownloadString(string url)
+    {
+      return MakeRequest(url, () => _webClient.DownloadData(url));
+    }
+
+    public string UploadValues(string url, NameValueCollection keyValuePairs)
+    {
+      return MakeRequest(url, () => _webClient.UploadValues(url, keyValuePairs));
+    }
+
+    #endregion
+
+    #region IUrlFetcher Members
 
     public string Fetch(string url)
     {
-      var fetchRequest = (HttpWebRequest)WebRequest.Create(url);      
-      
-      fetchRequest.Method = "GET";
-      
-      using (var resp = fetchRequest.GetResponse()) 
-      using (var reader = new StreamReader(resp.GetResponseStream(), true))
+      return DownloadString(url);
+    }
+
+    #endregion
+
+    #region Private helper methods
+
+    private static Encoding GetEncodingFromMetaTag(byte[] responseBytes)
+    {
+      string responseText = Encoding.ASCII.GetString(responseBytes);
+      Match match = _MetaTagRegex.Match(responseText);
+
+      if (match.Success)
       {
-        return reader.ReadToEnd();
+        string charset = match.Groups["charset"].Value;
+
+        try
+        {
+          return Encoding.GetEncoding(charset);
+        }
+        catch (ArgumentException)
+        {
+          return null;
+        }
       }
+
+      return null;
+    }
+
+    private static byte[] Decompress(byte[] responseBytes, CompressionAlgorithm compressionAlgorithm)
+    {
+      Stream decompressingStream;
+
+      switch (compressionAlgorithm)
+      {
+        case CompressionAlgorithm.GZip:
+          decompressingStream = new GZipStream(new MemoryStream(responseBytes), CompressionMode.Decompress);
+          break;
+
+        case CompressionAlgorithm.Deflate:
+          decompressingStream = new DeflateStream(new MemoryStream(responseBytes), CompressionMode.Decompress);
+          break;
+
+        default:
+          throw new NotSupportedException("Unsupported compression algorithm: " + compressionAlgorithm + ".");
+      }
+
+      try
+      {
+        byte[] buffer = new byte[BufferSize];
+
+        using (var outputMemoryStream = new MemoryStream())
+        {
+          while (true)
+          {
+            int bytesRead = decompressingStream.Read(buffer, 0, buffer.Length);
+
+            if (bytesRead <= 0)
+            {
+              break;
+            }
+
+            outputMemoryStream.Write(buffer, 0, bytesRead);
+          }
+
+          return outputMemoryStream.ToArray();
+        }
+      }
+      finally
+      {
+        decompressingStream.Close();
+      }
+    }
+
+    private string MakeRequest(string url, Func<byte[]> makeRequestFunc)
+    {
+      if (string.IsNullOrEmpty(url))
+      {
+        throw new ArgumentNullException("url");
+      }
+
+      byte[] responseBytes = makeRequestFunc();
+      string contentEncoding = _webClient.ResponseHeaders["Content-Encoding"];
+
+      if (!string.IsNullOrEmpty(contentEncoding))
+      {
+        contentEncoding = contentEncoding.ToLower();
+
+        if (contentEncoding.Contains("gzip"))
+        {
+          responseBytes = Decompress(responseBytes, CompressionAlgorithm.GZip);
+        }
+        else if (contentEncoding.Contains("deflate"))
+        {
+          responseBytes = Decompress(responseBytes, CompressionAlgorithm.Deflate);
+        }
+        else
+        {
+          throw new NotSupportedException("Unsupported content encoding: " + contentEncoding + ".");
+        }
+      }
+
+      Encoding encoding = GuessEncoding(_webClient.ResponseHeaders, responseBytes);
+
+      return encoding.GetString(responseBytes);
+    }
+
+    private Encoding GuessEncoding(WebHeaderCollection headers, byte[] responseBytes)
+    {
+      try
+      {
+        if (headers == null)
+        {
+          return _fallbackEncoding;
+        }
+
+        string contentType = headers[HttpResponseHeader.ContentType];
+
+        if (string.IsNullOrEmpty(contentType))
+        {
+          return _fallbackEncoding;
+        }
+
+        Encoding resultEncoding = null;
+        string[] splittedContentType = contentType.ToLower(CultureInfo.InvariantCulture).Split(new[] { ';', '=', ' ' });
+        bool isCharset = false;
+
+        foreach (string s in splittedContentType)
+        {
+          if (s == "charset")
+          {
+            isCharset = true;
+          }
+          else if (isCharset)
+          {
+            try
+            {
+              resultEncoding = Encoding.GetEncoding(s);
+            }
+            catch (ArgumentException)
+            {
+              // encoding not supported - we'll fallback later
+            }
+
+            break;
+          }
+        }
+
+        return resultEncoding ?? (GetEncodingFromMetaTag(responseBytes) ?? _fallbackEncoding);
+      }
+      catch (Exception exception)
+      {
+        if (((exception is ThreadAbortException) || (exception is StackOverflowException)) || (exception is OutOfMemoryException))
+        {
+          throw;
+        }
+      }
+
+      return _fallbackEncoding;
     }
 
     #endregion
