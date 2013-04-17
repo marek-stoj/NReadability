aa51285f9d730ba87e6af8bfd7b08641f65235e1
Immortal <marek.stoj@gmail.com>
2013-04-17 08:22:28 +0200

    Updated User-Agent header. Added Accept header because some sites require it.

--- Src/NReadability/NReadability/UrlFetcher.cs
+++ Src/NReadability/NReadability/UrlFetcher.cs
@@ -57,7 +57,8 @@ namespace NReadability
       _fallbackEncoding = fallbackEncoding ?? _DefaultFallbackEncoding;
 
       _webClient = new CookieAwareWebClient();
-      _webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:13.0) Gecko/20100101 Firefox/13.0.1");
+      _webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31");
+      _webClient.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
       _webClient.Headers.Add("Accept-Encoding", "gzip,deflate");
     }
 
